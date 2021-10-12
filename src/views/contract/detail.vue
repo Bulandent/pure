17ebd02e9771d6c6120248707694588385d8c69d
ccp <1326895569.com>
2021-10-12 08:09:38 +0800

    feat: 更新文件上传

--- src/views/contract/detail.vue
+++ src/views/contract/detail.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="contract-detail">
-		<el-tabs v-model="activeTab" @tab-click="handleClick">
+		<el-tabs v-model="activeTab">
 			<el-tab-pane label="模板信息" name="first"></el-tab-pane>
 		</el-tabs>
 		<div class="contract-first">
@@ -30,59 +30,58 @@
 						maxlength="80"
 					></el-input>
 				</el-form-item>
-				<el-form-item label="模板文件" prop="applyStartTime">
+				<el-form-item label="模板文件" prop="templateUrl">
 					<el-upload
 						class="upload-demo"
 						action=""
+						:disabled="isDisabled"
 						:on-change="handleTemplateFileChange"
 						:show-file-list="false"
+						accept=".doc,.docx,.xlsx,.pptx"
+						:before-upload="beforeTemplateUpload"
 					>
 						<el-input
 							type="text"
 							v-model="contractForm.templateUrl"
+							:disabled="isDisabled"
 							placeholder="点击上传"
 						></el-input>
 					</el-upload>
 				</el-form-item>
-				<el-form-item label="活动开始时间" prop="actStartTime">
-					<el-date-picker
-						type="datetime"
-						placeholder="选择时间"
-						v-model="contractForm.actStartTime"
+				<el-form-item label="预览文件" prop="previewUrl">
+					<el-upload
+						class="upload-demo"
+						action=""
 						:disabled="isDisabled"
-						style="width: 100%"
-					></el-date-picker>
+						:on-change="handlePreviewFileChange"
+						:show-file-list="false"
+						accept=".pdf"
+						:before-upload="beforePreviewUpload"
+					>
+						<el-input
+							type="text"
+							v-model="contractForm.previewUrl"
+							:disabled="isDisabled"
+							placeholder="点击上传"
+						></el-input>
+					</el-upload>
 				</el-form-item>
-				<el-form-item label="可参与人数" prop="countPerson">
+				<el-form-item label="售价" prop="soldPrice">
 					<el-col :span="21">
 						<el-input
 							type="text"
-							v-model="contractForm.countPerson"
+							v-model="contractForm.soldPrice"
 							:disabled="isDisabled"
 						></el-input>
 					</el-col>
-					<el-col :span="2" style="text-align: right">人</el-col>
+					<el-col :span="2" style="text-align: right">元</el-col>
 				</el-form-item>
-				<el-form-item label="活动地址" prop="address" style="float: right">
-					<el-input type="text" v-model="contractForm.address" :disabled="isDisabled"></el-input>
-				</el-form-item>
-				<el-form-item label="详情" prop="contractDetail" style="width: 100%">
-					<el-input
-						type="textarea"
-						v-model="contractForm.contractDetail"
+				<el-form-item label="状态" prop="status">
+					<el-switch
+						v-model="contractForm.status"
 						:disabled="isDisabled"
-						rows="4"
-					></el-input>
-				</el-form-item>
-				<el-form-item label="须知模板" prop="contractTemplate">
-					<el-select v-model="contractForm.contractTemplate" :disabled="isDisabled">
-						<el-option
-							v-for="item in templateList"
-							:label="item.label"
-							:value="item.value"
-							:key="item.value"
-						></el-option>
-					</el-select>
+						active-text="开启"
+					></el-switch>
 				</el-form-item>
 			</el-form>
 			<div class="action-area" v-if="(isEdit && isDetail) || !isDetail">
@@ -95,7 +94,7 @@
 
 <script>
 export default {
-	name: 'LectureDetail',
+	name: 'ContractDetail',
 	components: {},
 	data() {
 		return {
@@ -104,18 +103,14 @@ export default {
 			isDetail: false,
 			isRequesting: false,
 			isEdit: false,
-			applyCount: 10,
-			restCount: 12,
 			contractForm: {
+				templateName: null,
+				templateType: null,
+				templateDesc: null,
 				templateUrl: null,
-				contractName: null,
-				sponsor: null,
-				applyStartTime: null,
-				actStartTime: null,
-				countPerson: null,
-				address: null,
-				contractDetail: null,
-				contractTemplate: null,
+				previewUrl: null,
+				soldPrice: null,
+				status: null,
 			},
 			templateList: [
 				{
@@ -147,30 +142,14 @@ export default {
 					],
 				},
 			],
-			Total: 0,
-			tableData: [
-				{
-					applyName: '张娜',
-					mobile: '18908031120',
-					roleName: '客户',
-					countPerson: 1,
-					payment: 30,
-					applyTime: '2021.12.12 13:02',
-					platform: 'APP',
-				},
-			],
-			pageSize: 10,
-			page: 1,
 			rules: {
-				pictureUrl: [{ required: true, message: '必填', trigger: 'change' }],
-				contractName: [{ required: true, message: '必填', trigger: 'blur' }],
-				sponsor: [{ required: true, message: '必填', trigger: 'blur' }],
-				applyStartTime: [{ required: true, message: '必填', trigger: 'change' }],
-				actStartTime: [{ required: true, message: '必填', trigger: 'change' }],
-				address: [{ required: true, message: '必填', trigger: 'blur' }],
-				contractDetail: [{ required: true, message: '必填', trigger: 'blur' }],
-				contractTemplate: [{ required: true, message: '必填', trigger: 'change' }],
-				countPerson: [
+				templateName: [{ required: true, message: '必填', trigger: 'blur' }],
+				templateType: [{ required: true, message: '必填', trigger: 'change' }],
+				templateDesc: [{ required: true, message: '必填', trigger: 'blur' }],
+				templateUrl: [{ required: true, message: '必填', trigger: 'change' }],
+				previewUrl: [{ required: true, message: '必填', trigger: 'change' }],
+				status: [{ required: true, message: '必填', trigger: 'change' }],
+				soldPrice: [
 					{ required: true, message: '必填', trigger: 'blur' },
 					{ pattern: /(?:^[0-9]$)|(?:^[1-9][0-9]+$)/, message: '格式不正确', trigger: 'blur' },
 				],
@@ -183,7 +162,6 @@ export default {
 		},
 	},
 	created() {
-		console.log(this.$route)
 		this.isDetail = this.$route.path.includes('detail')
 		if (this.isDetail) {
 			this.contractNo = this.$route.query.contractNo
@@ -192,7 +170,12 @@ export default {
 		}
 	},
 	methods: {
-		handleTemplateFileChange(file, fileList) {},
+		handleTemplateFileChange(file, fileList) {
+			console.log(file, fileList)
+		},
+		handlePreviewFileChange(file, fileList) {
+			console.log(file, fileList)
+		},
 		getDetail() {
 			// 获取详情
 		},
@@ -203,14 +186,6 @@ export default {
 			this.$router.push({
 				path: '/contract/list',
 			})
-		},
-		handleSizeChange(e) {
-			this.pageSize = e
-			this.getApplyList()
-		},
-		currentChange(e) {
-			this.page = e
-			this.getApplyList()
 		},
 		submitForm() {
 			if (this.isRequesting) return
@@ -229,13 +204,21 @@ export default {
 		resetForm() {
 			this.$refs['contractForm'].resetFields()
 		},
-		handleClick(tab, event) {
-			console.log(tab, event)
+		beforeTemplateUpload(file) {
+			console.log(file)
+			const isJPG = file.type === 'image/jpeg'
+			const isLt2M = file.size / 1024 / 1024 < 2
+
+			if (!isJPG) {
+				this.$message.error('上传头像图片只能是 JPG 格式!')
+			}
+			if (!isLt2M) {
+				this.$message.error('上传头像图片大小不能超过 2MB!')
+			}
+			return isJPG && isLt2M
 		},
-		handlepictureSuccess(res, file) {
-			this.contractForm.picture = URL.createObjectURL(file.raw)
-		},
-		beforepictureUpload(file) {
+		beforePreviewUpload(file) {
+			console.log(file)
 			const isJPG = file.type === 'image/jpeg'
 			const isLt2M = file.size / 1024 / 1024 < 2
 
